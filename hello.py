5229c2fc0b288e6e10c13b381cc1537456a040ff
Carlos Moreno <omorenodovale@gmail.com>
2022-08-08 23:40:17 -0300

    Ajustes no script hello para dar mais opções de interação na definição da linguagem a ser impressa.

--- hello.py
+++ hello.py
@@ -9,18 +9,48 @@ Como usar:
 Tenha a variável LANG devidamente configurada, ex.:
     export LANG=pt_BR
 
+Ou informe por CLI argument `--lang`
+
+Ou o usuário terá que digitar.
+
 Execução:
     python3 hello.py
     ou
     ./hello.py
+    ou
+    python hello.py --lang=pt_BR
+    ou
+    ./hello.py --lang=pt_BR
 """
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __author__ = "Carlos Moreno"
 __license__ = "Unlicense"
 
 import os
+import sys
 
-current_language = os.getenv("LANG", "en_US.UTF-8").split(".")[0]
+
+arguments = {"lang": None, "count": 1}
+
+for arg in sys.argv[1:]:
+    # TODO: Tratar ValueError
+    key, value = arg.split("=")
+    key = key.lstrip("-").strip()
+    value = value.strip()
+    if key not in arguments:
+        print(f"Invalid option ==> {key}")
+    arguments[key] = value
+
+current_language = arguments["lang"]
+
+if current_language is None:
+    # TODO: Usar repetição
+    if "LANG" in os.environ:
+        current_language = os.getenv("LANG")
+    else:
+        current_language = input("Choose a language: ")
+
+current_language = current_language[:5]
 
 msg = {
     "en_US": "Hello, World!",
@@ -30,4 +60,6 @@ msg = {
     "fr_FR": "Bonjour Monde",
 }
 
-print(msg[current_language])
+print(
+    msg[current_language] * int(arguments["count"])
+)
