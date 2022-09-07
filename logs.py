d8f5d1dd8dfefad25f3a08705443c300a1c55900
Carlos Moreno <omorenodovale@gmail.com>
2022-09-07 17:15:28 -0300

    Rotating File handler logs

--- logs.py
+++ logs.py
@@ -2,6 +2,7 @@
 
 import os
 import logging
+from logging import handlers
 
 # BOILERPLATE
 # TODO: usar função
@@ -10,16 +11,20 @@ log_level = os.getenv("LOG_LEVEL", "WARNING").upper()
 
 log = logging.Logger(__name__, level=log_level)
 
-ch = logging.StreamHandler()
-ch.setLevel(log_level)
+# ch = logging.StreamHandler()
+# ch.setLevel(log_level)
+fh = handlers.RotatingFileHandler(
+    "meulog.log", maxBytes=10**6, backupCount=10
+)
+fh.setLevel(log_level)
 
 fmt = logging.Formatter(
     "%(asctime)s %(name)s %(levelname)s l:%(lineno)d "
     "f:%(filename)s: %(message)s"
 )
-ch.setFormatter(fmt)
+fh.setFormatter(fmt)
 
-log.addHandler(ch)
+log.addHandler(fh)
 
 """
 log.debug("Mensagem para o DEV, QE, SYSADMIN")
