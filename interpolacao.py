906f7be44dd98af35b5ec448ef51244baf47ed7a
carlos-moreno <omorenodovale@gmail.com>
2022-09-21 01:06:37 +0000

    Atualizando o script de interpolacao para usar a biblioteca smtplib

--- interpolacao.py
+++ interpolacao.py
@@ -8,6 +8,8 @@ __author__ = "Carlos Moreno"
 
 import sys
 import os
+import smtplib
+from email.mime.text import MIMEText
 
 arguments = sys.argv[1:]
 if not arguments:
@@ -25,20 +27,26 @@ path = os.curdir
 filepath = os.path.join(path, emails_file)
 templatepath = os.path.join(path, template_file)
 
-for line in open(filepath):
-    name, email = line.split(",")
+with smtplib.SMTP(host="localhost", port=8025) as server:
+    for line in open(filepath):
+        name, email = line.split(",")
+        text = (
+            open(templatepath).read()
+            % {
+                "nome": name,
+                "produto": "caneta",
+                "texto": "Escrever muito bem",
+                "link": "https://canetaslegais.com",
+                "quantidade": 1,
+                "preco": 50.5,
+            }
+        )
 
-    # TODO: Substituir por envio de e-mail
-    print(f"Enviando e-mail para: {email}")
-    print(
-        open(templatepath).read()
-        % {
-            "nome": name,
-            "produto": "caneta",
-            "texto": "Escrever muito bem",
-            "link": "https://canetaslegais.com",
-            "quantidade": 1,
-            "preco": 50.5,
-        }
-    )
-    print("-" * 50)
+        from_ = "beltrano@xpto.com"
+        to = ", ".join([email])
+        message = MIMEText(text)
+        message["Subject"] = "Compre mais"
+        message["From"] = from_
+        message["To"] = to
+
+        server.sendmail(from_, to, message.as_string())
