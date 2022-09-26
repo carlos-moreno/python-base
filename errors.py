d1f6c02ef89f1e88fbbc12fb1bdaf20b5f39ee19
Carlos Moreno <omorenodovale@gmail.com>
2022-09-26 16:21:52 -0300

    retry com recursão

--- errors.py
+++ errors.py
@@ -13,16 +13,20 @@ log = logging.Logger("errors")
 
 def try_to_open_a_file(filepath, retry=0):
     """Tries to open a file, if error, retries n times."""
-    for attempt in range(1, retry + 1):
-        try:
-            return open(filepath).readlines()
-        except FileNotFoundError as e:
-            log.error("ERROR: %s", e)
-            time.sleep(3)
-        else:
-            print("SUcesso!!!")
-        finally:
-            print("Execute isso sempre")
+    if retry > 999:
+        raise ValueError("Retry cannot be above 999")
+    try:
+        return open(filepath).readlines()
+    except FileNotFoundError as e:
+        log.error("ERROR: %s", e)
+        time.sleep(2)
+        if retry > 1:
+            return try_to_open_a_file(filepath, retry=retry - 1)
+    else:
+        print("SUcesso!!!")
+    finally:
+        print("Execute isso sempre")
+
     return []
 
 
